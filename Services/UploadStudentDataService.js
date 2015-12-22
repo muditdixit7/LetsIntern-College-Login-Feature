d9648d9118e6cf704d0f5954b0969c33ebcde348
muditdixit7 <muditdixit7@gmail.com>
2015-12-22 09:48:48 +0530

    Refactoring

--- Services/UploadStudentDataService.js
+++ Services/UploadStudentDataService.js
@@ -2,26 +2,28 @@ var exports = module.exports = {}
 var fs = require('fs')
 
 exports.Uploader = function(req, res) {
-  console.log(req)
-  console.log(req.files.file.name);
-  console.log(req.files.file.path);
-  console.log(req.files.file.type);
+	console.log(req)
+	//console.log(req.file.name);
+	console.log(req.files.file.path);
+	console.log(req.files.file.type);
 
-  var file = __dirname + "/" + req.files.file.name;
-  fs.readFile(req.files.file.path, function(err, data) {
-    //fs.writeFile(file, data, function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      response = {
-        message: 'File uploaded successfully',
-        filename: req.files.file.name
-      };
-    }
-    console.log(response);
-    res.end(JSON.stringify(response));
-    console.log(data)
-      //});
-    console.log(data)
-  });
+	var file = __dirname + "/" + req.files.file.name;
+	fs.readFile(req.files.file.path, function(err, data) {
+		/*fs.writeFile(file, data, function(err) {
+			if (err) {
+				console.log(err);
+			} else {
+				response = {
+					message: 'File uploaded successfully',
+					filename: req.files.file.name
+				};
+			}
+			console.log(response);
+			res.end(JSON.stringify(response));
+			console.log(data)
+				//});
+			console.log(data)
+		});*/
+		console.log(data)
+	})
 }
--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-var mongoose = require('mongoose')
-var bodyParser = require('body-parser')
-var express = require('express')
-var multer = require('multer');
-var path = require('path')
-var app = express();
+var mongoose = require('mongoose'),
+	bodyParser = require('body-parser'),
+	express = require('express'),
+	multer = require('multer'),
+	path = require('path'),
+	app = express(),
 
-var schemas = require(process.cwd() + '\\Database\\UserSchemas.js')
-var appConfig = require(process.cwd() + '\\AppConfig.js')
-var uploadStudentData = require(process.cwd() + '\\Services\\UploadStudentDataService.js')
-var publicRoutes = require(process.cwd() + '\\Routes\\PublicRoutes.js')
-var privateRoutes = require(process.cwd() + '\\Routes\\PrivateRoutes.js')
+	schemas = require(process.cwd() + '\\Database\\UserSchemas.js'),
+	appConfig = require(process.cwd() + '\\AppConfig.js'),
+	uploadStudentData = require(process.cwd() + '\\Services\\UploadStudentDataService.js'),
+	publicRoutes = require(process.cwd() + '\\Routes\\PublicRoutes.js'),
+	privateRoutes = require(process.cwd() + '\\Routes\\PrivateRoutes.js');
 
-console.log('app mei', appConfig.secret)
+
 app.set('superSecret', appConfig.secret)
-
-console.log(multer)
 
 
 app.use(bodyParser.urlencoded({
 	extended: false
-}));
+}))
 
 app.use(multer({
 	dest: './uploads/'
-}))
+}).array('file'));
+
 
 app.use('/public', publicRoutes.Router)
 app.use('/private', privateRoutes.Router)
@@ -36,7 +35,7 @@ app.post('/file_upload', uploadStudentData.Uploader)
 
 schemas.db.on('error', console.error.bind(console, 'connection error:'));
 schemas.db.once('open', function() {
-	var server = app.listen(8087, function(err) {
+	var server = app.listen(8082, function(err) {
 		if (err) throw err
 		else {
 			var host = server.address().address
